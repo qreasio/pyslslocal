511aa1009959e7ab154ac3b155fd277136929f9e
isakfm <isak.rickyanto@gmail.com>
2020-07-06 21:31:08 +0700

    add stepfunctions and local stepfunctions offline test

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import os
 import boto3
 import json
+import time
 import unittest
 
-LAMBDA_ENDPOINT='http://localhost:3002'
-REGION='us-east-1'
-STATE_MACHINE_ARN='arn:aws:states:us-east-1:123456789:stateMachine:foo'
+LAMBDA_ENDPOINT = 'http://localhost:3000'
+REGION = 'us-east-1'
+STATE_MACHINE_ARN = 'arn:aws:states:us-east-1:123456789:stateMachine:foo'
+SFN_ENDPOINT = 'http://localhost:4584'
+
 
 class TestStatus(unittest.TestCase):
     def setUp(self):
@@ -17,27 +20,35 @@ class TestStatus(unittest.TestCase):
         os.system(cmd)
 
     def test_hello(self):
-        client = boto3.client('lambda', endpoint_url=LAMBDA_ENDPOINT, region_name=REGION)
-        response = client.invoke(
-            FunctionName='theFirst',
-            InvocationType = 'RequestResponse',
-            Payload = "{}"
-        )
+        client = boto3.client('lambda',
+                              endpoint_url=LAMBDA_ENDPOINT,
+                              region_name=REGION)
+        response = client.invoke(FunctionName='theFirst',
+                                 InvocationType='RequestResponse',
+                                 Payload="{}")
 
         res_json = json.loads(response['Payload'].read().decode("utf-8"))
         self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], 200)
         self.assertEqual(res_json['statusCode'], 200)
 
-        response = client.invoke(
-            FunctionName='theSecond',
-            InvocationType = 'RequestResponse',
-            Payload = "{}"
-        )
+        sfn = boto3.client('stepfunctions',
+                           region_name=REGION,
+                           endpoint_url=SFN_ENDPOINT)
 
-        res_json = json.loads(response['Payload'].read().decode("utf-8"))
-        self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], 200)
-        self.assertEqual(res_json['statusCode'], 200)
+        sm_arn = 'arn:aws:states:us-east-1:123456789:stateMachine:foo'
+
+        execution = sfn.start_execution(stateMachineArn=sm_arn)
+
+        response = sfn.list_executions(stateMachineArn=sm_arn)
+
+        status = sfn.describe_execution(executionArn=execution["executionArn"])
+
+        time.sleep(5)
+
+        history = sfn.get_execution_history(
+            executionArn=execution["executionArn"], reverseOrder=True)
+        print(history)
 
 
-if __name__ == '__main__': 
-    unittest.main() 
+if __name__ == '__main__':
+    unittest.main()
